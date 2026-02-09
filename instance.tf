99ab5673a225eeb8b65d9787698fca864128c6ff
AviTE86 <satmyhat@gmail.com>
2026-02-09 14:47:52 +0200

    Create instance.tf
    1

---
+++ instance.tf
@@ -0,0 +1,61 @@
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name   = "web-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+# Ubuntu 22.04 AMI lookup
+data "aws_ami" "ubuntu_22" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ubuntu_22.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "public-web-instance"
+  }
+}
+
+# Output public IP
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
